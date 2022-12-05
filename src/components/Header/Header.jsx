f5a1b8b3385e5c3e0561ae5fd4e4d53c2df0e3c2
theborgh <borghiborghi@gmail.com>
2022-12-05 11:23:26 +0100

    add simple navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./header.scss";
 
 export default function Header() {
-  return <div className="text-3xl font-bold underline">Header</div>;
+  return (
+    <div className="flex bg-header-bg p-2 place-content-between">
+      <div id="logo">
+        <Link className="logo font-black" to={"/"}>
+          <span className="text-logo1">OpenAI</span>
+          <span className="text-logo2">Demo</span>
+        </Link>
+      </div>
+      <div id="links" className="flex justify-end space-x-2">
+        <div className="navbarLink text-logo1">
+          <Link to={"/about"}>About</Link>
+        </div>
+        <div className="navbarLink">Sign In</div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "header-bg": "#8ecd8e",
+        "header-link": "",
+        "footer-bg": "#71c171",
+        logo1: "#47476b",
+        logo2: "#666699",
+      },
+    },
   },
   plugins: [],
 };
