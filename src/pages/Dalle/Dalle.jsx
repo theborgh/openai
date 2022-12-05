746c62fd999cdaf59d72c7c3e4138aee94f4e6d1
theborgh <borghiborghi@gmail.com>
2022-12-05 17:13:01 +0100

    bypass openai library, call api directly

--- src/pages/Dalle/Dalle.jsx
+++ src/pages/Dalle/Dalle.jsx
@@ -1,36 +1,44 @@
-import React, { useEffect } from "react";
-import { Configuration, OpenAIApi } from "openai";
+import React, { useState } from "react";
+import ResultCard from "../../components/ResultCard/ResultCard";
 
 export default function Dalle() {
-  const configuration = new Configuration({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  });
-  const oai = new OpenAIApi(configuration);
+  const [prompt, setPrompt] = useState("");
+  const [results, setResults] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const userPrompt = e.target.elements[0].value;
 
-    if (userPrompt.length < 10 || userPrompt.split(" ").length < 3) {
-      console.warn("Prompt is very short, not ideal");
-    } else {
-      console.log("Will generate image");
-      generateImage(oai, userPrompt);
-    }
+    generateImage();
   };
 
-  const generateImage = async (oai, userPrompt) => {
-    const response = await oai.createImage({
-      prompt: userPrompt,
+  const generateImage = async () => {
+    console.log("prompt: ", prompt);
+
+    const params = {
+      prompt,
       n: 1,
       size: "1024x1024",
-    });
+    };
 
-    const image_url = response.data.data[0].url;
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + String(import.meta.env.VITE_OPENAI_API_KEY),
+      },
+      body: JSON.stringify(params),
+    };
+
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      requestOptions
+    );
+
+    const image_url = response.data;
     console.log("The url is: ", image_url);
-  };
 
-  useEffect(() => {}, []);
+    console.log("test log: generateImage");
+  };
 
   return (
     <>
@@ -46,17 +54,32 @@ export default function Dalle() {
           onSubmit={handleSubmit}
         >
           <input
+            onChange={(e) => setPrompt(e.target.value)}
             type="text"
             id="description"
             className="w-full form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            value={prompt}
           />
+          <div
+            onClick={(e) => setPrompt("")}
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          >
+            Clear
+          </div>
           <button
             role="submit"
             className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            disabled={prompt.length < 10 || prompt.split(" ").length < 3}
           >
             Submit
           </button>
         </form>
+
+        <div className="flex mt-5">
+          {results.map((result) => (
+            <ResultCard key={result} resultUrl={result} />
+          ))}
+        </div>
       </div>
     </>
   );
