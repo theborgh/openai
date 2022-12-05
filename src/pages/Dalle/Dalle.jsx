4bade53a5f23fcfe9c3546d81ca5bf1001045f5c
theborgh <borghiborghi@gmail.com>
2022-12-05 16:29:16 +0100

    Add basic dalle flow

--- src/pages/Dalle/Dalle.jsx
+++ src/pages/Dalle/Dalle.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Configuration, OpenAIApi } from "openai";
 
 export default function Dalle() {
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  });
+  const oai = new OpenAIApi(configuration);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userPrompt = e.target.elements[0].value;
+
+    if (userPrompt.length < 10 || userPrompt.split(" ").length < 3) {
+      console.warn("Prompt is very short, not ideal");
+    } else {
+      console.log("Will generate image");
+      generateImage(oai, userPrompt);
+    }
+  };
+
+  const generateImage = async (oai, userPrompt) => {
+    const response = await oai.createImage({
+      prompt: userPrompt,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    const image_url = response.data.data[0].url;
+    console.log("The url is: ", image_url);
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <h1 className="text-center">DALL-E</h1>
@@ -10,7 +41,10 @@ export default function Dalle() {
       </p>
 
       <div className="mt-3">
-        <form className="flex place-content-center">
+        <form
+          className="flex place-content-center gap-2 mx-5"
+          onSubmit={handleSubmit}
+        >
           <input
             type="text"
             id="description"
