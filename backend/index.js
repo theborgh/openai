28daa5129fe3065dbff6a5a632a188f466b0ca77
theborgh <borghiborghi@gmail.com>
2022-12-06 10:06:02 +0100

    add initial cloudinary config

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ require("dotenv").config({ path: `${__dirname}/.env` });
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>mWelcome</h1>");
+  res.send("<h1>Welcome</h1>");
 });
 
 app.listen(PORT, console.log(`listening on PORT: ${PORT}`));
--- frontend/src/pages/Dalle/Dalle.jsx
+++ frontend/src/pages/Dalle/Dalle.jsx
@@ -66,8 +66,6 @@ export default function Dalle() {
         description: prompt,
       }));
 
-      console.log("newData: ", newData);
-
       setResults([...newData, ...results]);
       localStorage.setItem("urls", results.join("#"));
     }
@@ -76,12 +74,8 @@ export default function Dalle() {
   return (
     <>
       <h1 className="text-center">DALL-E</h1>
-      <p className="text-sm">
-        Type a natural language description of the image you would like to see.
-        You will get a downloadable version of it in 1024x1024px resolution.
-      </p>
 
-      <div className="mt-3">
+      <div className="mt-6">
         <form
           className="flex place-content-center gap-2 mx-5"
           onSubmit={handleSubmit}
@@ -92,16 +86,17 @@ export default function Dalle() {
             id="description"
             className="w-full form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
             value={prompt}
+            placeholder="Type a natural language description of the image you would like to see &ndash; the more descriptive, the better"
           />
           <div
             onClick={() => setPrompt("")}
-            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            className="bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent hover:cursor-pointer"
           >
             Clear
           </div>
           <button
             role="submit"
-            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            className="bg-transparent text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:bg-blue-500 hover:border-transparent rounded disabled:text-indigo-200 disabled:border-indigo-200 disabled:hover:text-indigo-200 disabled:hover:bg-white"
             disabled={prompt.length < 10 || prompt.split(" ").length < 3}
           >
             Submit
