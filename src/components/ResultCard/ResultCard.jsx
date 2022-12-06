52ee4fb4fe85c6a67ff694924093e6fcff3597fa
theborgh <borghiborghi@gmail.com>
2022-12-06 01:02:53 +0100

    fix image description

--- src/components/ResultCard/ResultCard.jsx
+++ src/components/ResultCard/ResultCard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./ResultCard.scss";
 
 export default function ResultCard({ resultUrl, description, handleDelete }) {
   return (
-    <div className="cardContainer bg-slate-200">
-      <a href={resultUrl} target="_blank">
+    <div className="cardContainer bg-slate-200 w-64">
+      <a href={resultUrl} className="cardImage" target="_blank">
         <img src={resultUrl} className="h-64 hover:border-2" />
       </a>
-      <div className="description">{description}</div>
+      <div className="cardDescription -translate-y-48 w-64">{description}</div>
       <button onClick={() => handleDelete(resultUrl)}>Delete</button>
     </div>
   );
