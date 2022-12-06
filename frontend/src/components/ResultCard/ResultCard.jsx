bd83d8d0b22390b6d5c5bcf0efcc519f95e78954
theborgh <borghiborghi@gmail.com>
2022-12-06 22:23:43 +0100

    add thumbnail urls for cloudinary links

--- frontend/src/components/ResultCard/ResultCard.jsx
+++ frontend/src/components/ResultCard/ResultCard.jsx
@@ -4,10 +4,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./ResultCard.scss";
 
 export default function ResultCard({ resultUrl, description, handleDelete }) {
+  const generateCloudinaryThumbnailUrl = (url) => {
+    if (!url.includes("cloudinary")) return url;
+
+    let res = url.split("/");
+    res.splice(6, 0, "c_fill,g_auto,h_256");
+
+    return res.join("/");
+  };
+
   return (
     <div className="cardContainer bg-slate-200 w-64">
       <a href={resultUrl} className="cardImage" target="_blank">
-        <img src={resultUrl} className="h-64 hover:border-2" />
+        <img
+          src={generateCloudinaryThumbnailUrl(resultUrl)}
+          className="h-64 hover:border-2"
+        />
       </a>
       <div className="cardDescription -translate-y-48 w-64">{description}</div>
       <button
