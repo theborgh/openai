c91a575ef14eb263879b2564bd4a96c217e6c657
theborgh <borghiborghi@gmail.com>
2022-12-06 14:57:10 +0100

    upload first image to cloudinary

--- backend/controllers/dalle.js
+++ backend/controllers/dalle.js
@@ -1,11 +1,19 @@
-const processNewImages = async (req, res) => {
-  console.log("processNewImages: req.body is ", req.body);
+const cloudinary = require("../cloudinaryConfig");
 
-  res
-    .status(200)
-    .json(
-      "executed successfully, returning cloudinary URLs to store in localStorage & state"
-    );
+const processNewImages = async (req, res) => {
+  if (req.body && req.body.length !== 0) {
+    console.log("req.body[0].url is", req.body[0]);
+
+    const result = await cloudinary.uploader.upload(req.body[0].url, {
+      folder: "dalle",
+    });
+
+    console.log("uploaded, response: ", result);
+    req.body.forEach((el) => {});
+    res.status(200).json({ url: result.url, id: result.asset_id });
+  } else {
+    res.status(400).json("Some error occurred");
+  }
 };
 
 module.exports = { processNewImages };
