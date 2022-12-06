1833e6ecb432bd724eb3e813056f0a010f7376aa
theborgh <borghiborghi@gmail.com>
2022-12-06 02:09:19 +0100

    add basic BE structure

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+require("dotenv").config({ path: `${__dirname}/.env` });
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>mWelcome</h1>");
+});
+
+app.listen(PORT, console.log(`listening on PORT: ${PORT}`));
