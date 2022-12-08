16ec9f0d2cee22a457d7c18a8ecb49820bcd67c1
theborgh <borghiborghi@gmail.com>
2022-12-08 18:15:59 +0100

    improve login and signup forms

--- frontend/src/pages/LogIn/LogIn.jsx
+++ frontend/src/pages/LogIn/LogIn.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../../../firebaseConfig";
 
 export default function LogIn() {
@@ -36,22 +40,51 @@ export default function LogIn() {
       });
   };
 
+  const emailAndPWSignIn = ({ email, password, rememberme }) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        const loginData = {
+          email: user.email,
+          loginID: user.uid,
+        };
+
+        console.log("login data: ", loginData);
+
+        if (rememberme) {
+          window.localStorage.setItem("uid", loginData.loginID);
+          window.sessionStorage.removeItem("uid");
+        } else {
+          window.sessionStorage.setItem("uid", loginData.loginID);
+          window.localStorage.removeItem("uid");
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("Sign in Error: ", errorCode);
+      });
+  };
+
   return (
     <div>
       <h1 className="text-center text-color-primary mt-4">User login</h1>
 
       <div className="signup-wrapper flex flex-row-reverse content-center justify-items-center 0 h-full">
-        <div className="signup-form-container py-20 p-16 flex flex-col items-center	 justify-center sm:w-full ">
+        <div className="signup-form-container py-20 p-16 flex flex-col items-center justify-center sm:w-full ">
           <button
-            className="social-signup  mt-6 text-center social-button"
+            className="border-2 p-2 mt-6 text-center flex gap-2 hover:bg-color-disabled"
             onClick={googleSignIn}
           >
-            Login with Google
+            <img src="../../../assets/googlelogo.png" width="20px" />
+            <span className="text-sm">Sign in with Google</span>
           </button>
-          <form className="mt-5">
+          <div className="mt-3">or login below</div>
+          <form onSubmit={emailAndPWSignIn} className="mt-3">
             <div className="form-item">
               <input
-                className="form_input"
+                className="form-control block px-3 py-1.5 text-base font-normal text-color-disabled bg-white bg-clip-padding border border-solid border-color-primary rounded m-0 focus:text-color-primary focus:bg-white focus:border-color-primary focus:outline-none mb-1"
                 type="email"
                 id="email"
                 name="email"
@@ -60,7 +93,7 @@ export default function LogIn() {
             </div>
             <div className="form-item">
               <input
-                className="form_input"
+                className="form-control block px-3 py-1.5 text-base font-normal text-color-disabled bg-white bg-clip-padding border border-solid border-color-primary rounded m-0 focus:text-color-primary focus:bg-white focus:border-color-primary focus:outline-none"
                 type="password"
                 id="password"
                 name="password"
@@ -74,28 +107,36 @@ export default function LogIn() {
               name="rememberme"
             />
             <label
-              className="font-bold text-gray-500 text-sm"
+              className="font-bold text-color-primary text-sm"
               htmlFor="rememberme"
             >
               Remember me
             </label>
-            <div className="cta">
-              <button className="signup-btn mt-6" type="submit">
+            <div className="flex place-content-center">
+              <button
+                className="bg-transparent text-color-primary font-semibold hover:text-white py-2 px-4 border border-color-primary hover:bg-color-primary hover:border-transparent rounded disabled:text-color-disabled disabled:border-color-disabled disabled:hover:text-color-disabled disabled:hover:bg-white grow lg:grow-0 mt-2 "
+                type="submit"
+              >
                 Sign In
               </button>
               {/* <p className="text-right mt-2">forgot password?</p> */}
             </div>
           </form>
+          <div className="mt-4 text-sm">
+            Not registered?{" "}
+            <Link
+              to="/register"
+              className="text-color-primary font-bold hover:text-color-secondary"
+            >
+              Sign up
+            </Link>
+          </div>
         </div>
         <div className="form-image-background w-5/12 bg-students-bg relative  justify-center hidden md:flex">
           <p className="text-5xl text-center mt-11">
             See what AI is capable of
           </p>
         </div>
-      </div>
-
-      <div>
-        Not registered? <Link to="/register">Sign up</Link>
       </div>
     </div>
   );
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,40 +1,115 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../../firebaseConfig";
 
 export default function SignUp() {
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+
+        const userData = {
+          email: user.email,
+          name: user.displayName,
+          image: user.photoURL,
+          loginID: user.uid,
+        };
+
+        console.log("+ user data: ", userData);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(error, error.message);
+      });
+  };
+
+  const emailAndPWSignUp = (e) => {
+    e.preventDefault();
+    const { username, email, password, rememberme } = e.target.elements;
+
+    const data = {
+      username: username.value,
+      email: email.value,
+      password: password.value,
+      rememberme: rememberme.checked,
+    };
+
+    console.log("auth: ", auth, data.email, data.password);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        const signUpData = {
+          username,
+          email: user.email,
+          loginID: user.uid,
+        };
+        console.log(
+          "+ createUserWithEmailAndPassword: ",
+          signUpData,
+          typeof signUpData.loginID
+        );
+
+        if (rememberme) {
+          window.localStorage.setItem("uid", loginData.loginID);
+          window.sessionStorage.removeItem("uid");
+        } else {
+          window.sessionStorage.setItem("uid", loginData.loginID);
+          window.localStorage.removeItem("uid");
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("Sign in Error: ", errorCode, errorMessage);
+      });
+  };
+
   return (
     <div>
       <h1 className="text-center text-color-primary mt-4">User registration</h1>
 
       <div className="signup-wrapper flex flex-row-reverse content-center justify-items-center 0 h-full">
-        <div className="signup-form-container py-20 p-16 flex flex-col items-center	 justify-center sm:w-full ">
+        <div className="signup-form-container py-20 p-16 flex flex-col items-center justify-center sm:w-full ">
           <button
-            className="social-signup  mt-6 social-button"
-            // onClick={googleSignUp}
+            className="border-2 p-2 mt-6 text-center flex gap-2 hover:bg-color-disabled"
+            onClick={googleSignIn}
           >
-            Sign In With Google
+            <img src="../../../assets/googlelogo.png" width="20px" />
+            <span className="text-sm">Sign in with Google</span>
           </button>
-          <p className="heading-text text-xl mt-6">Or Sign Up via email</p>
-          <form className="mt-5">
-            <div className="form-item flex gap-2">
+          <div className="mt-3">or register below</div>
+          <form onSubmit={emailAndPWSignUp} className="mt-3">
+            <div className="form-item">
               <input
-                className="form_input w-1/2"
+                className="form-control block px-3 py-1.5 text-base font-normal text-color-disabled bg-white bg-clip-padding border border-solid border-color-primary rounded m-0 focus:text-color-primary focus:bg-white focus:border-color-primary focus:outline-none mb-1"
                 type="text"
-                id="firstName"
-                name="firstName"
-                placeholder="first name"
-              />
-              <input
-                className="form_input w-1/2"
-                type="text"
-                id="lastName"
-                name="lastName"
-                placeholder="last name"
+                id="username"
+                name="username"
+                placeholder="Username"
               />
             </div>
             <div className="form-item">
               <input
-                className="form_input"
+                className="form-control block px-3 py-1.5 text-base font-normal text-color-disabled bg-white bg-clip-padding border border-solid border-color-primary rounded m-0 focus:text-color-primary focus:bg-white focus:border-color-primary focus:outline-none mb-1"
                 type="email"
                 id="email"
                 name="email"
@@ -43,7 +118,7 @@ export default function SignUp() {
             </div>
             <div className="form-item">
               <input
-                className="form_input"
+                className="form-control block px-3 py-1.5 text-base font-normal text-color-disabled bg-white bg-clip-padding border border-solid border-color-primary rounded m-0 focus:text-color-primary focus:bg-white focus:border-color-primary focus:outline-none"
                 type="password"
                 id="password"
                 name="password"
@@ -57,30 +132,36 @@ export default function SignUp() {
               name="rememberme"
             />
             <label
-              className="font-bold text-gray-500 text-sm"
+              className="font-bold text-color-primary text-sm"
               htmlFor="rememberme"
             >
               Remember me
             </label>
-            <div className="cta">
-              <button className="signup-btn mt-6" type="submit">
+            <div className="flex place-content-center">
+              <button
+                className="bg-transparent text-color-primary font-semibold hover:text-white py-2 px-4 border border-color-primary hover:bg-color-primary hover:border-transparent rounded disabled:text-color-disabled disabled:border-color-disabled disabled:hover:text-color-disabled disabled:hover:bg-white grow lg:grow-0 mt-2 "
+                type="submit"
+              >
                 Sign Up
               </button>
               {/* <p className="text-right mt-2">forgot password?</p> */}
             </div>
           </form>
+          <div className="mt-4 text-sm">
+            Already registered?{" "}
+            <Link
+              to="/login"
+              className="text-color-primary font-bold hover:text-color-secondary"
+            >
+              Sign in
+            </Link>
+          </div>
         </div>
         <div className="form-image-background w-5/12 bg-students-bg relative  justify-center hidden md:flex">
           <p className="text-5xl text-center mt-11">
             See what AI is capable of
           </p>
-
-          {/* <img src={stars} className="w-full h-full absolute z-55 top-0 " /> */}
         </div>
-      </div>
-
-      <div>
-        Already registered? <Link to="/login">Sign in</Link>
       </div>
     </div>
   );
